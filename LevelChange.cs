3ed8ff32fcc961ea5aa232d93a906ba256dffd7f
jon chambers <jon@jonchambers.net>
2019-04-02 20:57:52 -0500

    Add files via upload
    Audio and Level Change Collision

---
+++ LevelChange.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class LevelChange : MonoBehaviour
+{
+    public string LoadLevel;
+
+    void OnCollisionEnter(Collision collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(LoadLevel);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
